e1f2e4bfc2a0111845b3c67d26e6fbfde4af129d
xulm <xulm@bf.com>
2019-03-10 21:22:16 +0800

    update

---
+++ app/src/main/java/son/scroll/ScrollByActivity.java
@@ -0,0 +1,21 @@
+package son.scroll;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import son.dualai.R;
+
+/**
+ * Created on 2019/3/10.
+ */
+public class ScrollByActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollby);
+
+    }
+
+
+}
